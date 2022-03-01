83acb6783356b8cf8ff1ae7d65ac50b8ff71ea73
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 17:23:08 +0900

    4. 데이터 확인하기(toString)

--- DateStructure/src/list/arraylist/implementation/ArrayList.java
+++ DateStructure/src/list/arraylist/implementation/ArrayList.java
@@ -19,7 +19,7 @@ public class ArrayList {
     
     public boolean add(int index, Object element) {
     	// 엘리먼트 중간에 데이터를 추가하기 위해서는 끝의 엘리먼트부터 index의 노드까지 뒤로 한 칸씩 이동시켜야 함.
-    	for (int i=size-1; i<=index; i--) {
+    	for (int i=size-1; i>=index; i--) {
     		elementData[i+1] = elementData[i];
     	}
     	elementData[index] = element; // index에 노드 추가
@@ -28,14 +28,27 @@ public class ArrayList {
     	
     } // end of add
     
-    public Object get(int index) {
-    	return elementData[index];
-    	// arrayList의 장점이 get에서 나타난다.
-    	// arrayList는 내부적으로 배열을 사용한다. 
-    	// 그리고 그 배열에다가 index값을 줘서 특정한 위치에 있는 element를 가져오기 때문에
-    	// 배열의 인덱스로 접근한다는 장점이 생긴다.
-    	// 즉 arrayList로 구현한 경우, 특정한 위치에 있는 데이터를 가져오는게 굉장히 빠르다. 
-    }
+    public String toString() {
+    	String str = "[";
+    	for(int i=0; i<size; i++) {
+    		str += elementData[i];
+    		if(i < size-1) {
+    			str += ",";
+    		} // end of if
+    	} // end of for
+    	
+    	return str + "]";
+    	
+    } // end of toString
+    
+//    public Object get(int index) {
+//    	return elementData[index];
+//    	// arrayList의 장점이 get에서 나타난다.
+//    	// arrayList는 내부적으로 배열을 사용한다. 
+//    	// 그리고 그 배열에다가 index값을 줘서 특정한 위치에 있는 element를 가져오기 때문에
+//    	// 배열의 인덱스로 접근한다는 장점이 생긴다.
+//    	// 즉 arrayList로 구현한 경우, 특정한 위치에 있는 데이터를 가져오는게 굉장히 빠르다. 
+//    } // end of get
     
     
 } // end of class
--- DateStructure/src/list/arraylist/implementation/Main.java
+++ DateStructure/src/list/arraylist/implementation/Main.java
@@ -10,16 +10,17 @@ public class Main {
     	numbers.addLast(20);
     	numbers.addLast(30);
     	numbers.addLast(40);
-//    	// 중간 위치에 데이터 추가
-//    	numbers.add(1, 15);
-//    	// 첫번째 위치에 데이터 추가
-//    	numbers.addFirst(5);
+    	// 중간 위치에 데이터 추가
+    	numbers.add(1, 15);
+    	// 첫번째 위치에 데이터 추가
+    	numbers.addFirst(5);
     	
+    	System.out.println(numbers);
     	
-    	System.out.println(numbers.get(0));
-    	System.out.println(numbers.get(1));
-    	System.out.println(numbers.get(2));
-    	System.out.println(numbers.get(3));
+//    	System.out.println(numbers.get(0));
+//    	System.out.println(numbers.get(1));
+//    	System.out.println(numbers.get(2));
+//    	System.out.println(numbers.get(3));
     	
     } // end of main
 
