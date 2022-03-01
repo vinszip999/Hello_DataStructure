dabd96b156f3b803707ff3db7b7d850ab962de01
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 16:51:18 +0900

    2. 데이터 추가하기(add, addFirst)

--- DateStructure/src/list/arraylist/implementation/ArrayList.java
+++ DateStructure/src/list/arraylist/implementation/ArrayList.java
@@ -4,9 +4,28 @@ public class ArrayList {
     private int size = 0;
     private Object[] elementData = new Object[100];
     
+    public boolean addFirst(Object element) {
+    	return add(0, element);
+    	// add가 0번째 위치에 데이터를 추가하면서 뒤로 있는 element들을 한 칸씩 쭉 밀어내는 역할까지 수행함.
+    	
+    } // end of addFirst
+    
     public boolean addLast(Object element) {
     	elementData[size] = element;
     	size++;
     	return true;
-    }
-} 
+    	
+    } // end of addLast
+    
+    public boolean add(int index, Object element) {
+    	// 엘리먼트 중간에 데이터를 추가하기 위해서는 끝의 엘리먼트부터 index의 노드까지 뒤로 한 칸씩 이동시켜야 함.
+    	for (int i=size-1; i<=index; i--) {
+    		elementData[i+1] = elementData[i];
+    	}
+    	elementData[index] = element; // index에 노드 추가
+    	size++; // 엘리먼트 숫자 1 증가
+    	return true;
+    	
+    } // end of add
+    
+} // end of class
--- DateStructure/src/list/arraylist/implementation/Main.java
+++ DateStructure/src/list/arraylist/implementation/Main.java
@@ -5,10 +5,16 @@ public class Main {
     public static void main(String[] args) {
     	// ArrayList.java 파일의 클래스 인스턴스화
     	ArrayList numbers = new ArrayList(); 
+    	// 마지막 위치에 데이터 추가
     	numbers.addLast(10);
     	numbers.addLast(20);
     	numbers.addLast(30);
     	numbers.addLast(40);
-    }
+    	// 중간 위치에 데이터 추가
+    	numbers.add(1, 15);
+    	// 첫번째 위치에 데이터 추가
+    	numbers.addFirst(5);
+    	
+    } // end of main
 
-} 
+} // end of class
