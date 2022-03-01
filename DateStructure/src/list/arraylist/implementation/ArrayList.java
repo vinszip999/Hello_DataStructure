eb5fa961ff6f7df7f1575bc9f62d101ebad46f30
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 15:53:00 +0900

    1. 객체 생성하기

---
+++ DateStructure/src/list/arraylist/implementation/ArrayList.java
@@ -0,0 +1,6 @@
+package list.arraylist.implementation;
+
+public class ArrayList {
+    private int size = 0;
+    private Object[] elementData = new Object[100];
+} 
---
+++ DateStructure/src/list/arraylist/implementation/Main.java
@@ -0,0 +1,12 @@
+package list.arraylist.implementation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	// ArrayList.java 파일의 클래스 인스턴스화
+    	ArrayList numbers = new ArrayList(); 
+
+    }
+
+} 
