0e8977532b4e3a874d37f5d42361d00599937fe0
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-01 16:24:32 +0900

    2. 데이터 추가하기(addLast)

--- DateStructure/src/list/arraylist/implementation/ArrayList.java
+++ DateStructure/src/list/arraylist/implementation/ArrayList.java
@@ -3,4 +3,10 @@ package list.arraylist.implementation;
 public class ArrayList {
     private int size = 0;
     private Object[] elementData = new Object[100];
+    
+    public boolean addLast(Object element) {
+    	elementData[size] = element;
+    	size++;
+    	return true;
+    }
 } 
--- DateStructure/src/list/arraylist/implementation/Main.java
+++ DateStructure/src/list/arraylist/implementation/Main.java
@@ -3,10 +3,12 @@ package list.arraylist.implementation;
 public class Main {
 
     public static void main(String[] args) {
-
     	// ArrayList.java 파일의 클래스 인스턴스화
     	ArrayList numbers = new ArrayList(); 
-
+    	numbers.addLast(10);
+    	numbers.addLast(20);
+    	numbers.addLast(30);
+    	numbers.addLast(40);
     }
 
 } 
